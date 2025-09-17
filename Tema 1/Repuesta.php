77dd0f3d238d003fe0ebc146daa9227bc27ba73b
Hector <hmarmar504@g.educaand.es>
2025-09-17 14:42:38 +0200

    Primer ejercicio

---
+++ Tema 1/Repuesta.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resupuesta</title>
+</head>
+<body>
+    <?php 
+    $nombre = $_REQUEST["nombre"];
+    $apellidos = $_REQUEST["apellidos"];
+    $nombreCompleto = $nombre." ". $apellidos;
+    $correo = $_REQUEST["email"];
+    $telefono = $_REQUEST["telefono"];
+    $seleccion = $_REQUEST["Seleccion"];
+    $radio = $_REQUEST["Radio"];
+    $chboxs = $_REQUEST["chbox"];
+
+
+    
+    
+
+    print "<h1>Los datos introducidos son:</h1><br>";
+    echo "<h2>Nombre completo: " .$nombreCompleto."</h2>";
+    echo "<p>sus opciones son: </p>";
+
+    $n = count ($chboxs);
+    for ($i=0; $i<$n; $i++)
+        echo ($chboxs[$i]."<BR>\n");
+    ?>
+</body>
+</html>
